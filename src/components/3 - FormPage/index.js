f3d3b261e82591a6164a212984441868ee6b9983
Roberto Figliè <88441442+onirob@users.noreply.github.com>
2021-09-27 17:32:01 +0200

    taxonomy json example

--- src/components/3 - FormPage/index.js
+++ src/components/3 - FormPage/index.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
+import { Link as RouterLink } from "react-router-dom";
 import { Paper, makeStyles } from "@material-ui/core";
 import AddArticleForm from "../AddArticleForm";
+import StandardButton from "../StandardButton";
 
 const useStyles = makeStyles((theme) => ({
   pageContent: {
@@ -14,6 +16,14 @@ const FormPage = () => {
 
   return (
     <>
+      <StandardButton
+        variant="outlined"
+        text="Go Back"
+        size="small"
+        color="default"
+        component={RouterLink}
+        to="/"
+      />
       <Paper className={classes.pageContent}>
         <AddArticleForm />
       </Paper>
