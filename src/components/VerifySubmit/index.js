cd0fd905d35160b07a78f8c1004c0f6395f20095
Giusy Nittoli <giusynittoli@gmail.com>
2023-08-24 14:47:33 +0200

    Update index.js

--- src/components/VerifySubmit/index.js
+++ src/components/VerifySubmit/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { useForm } from '../../hooks/useForm';
 import { FormContext } from '../3 - FormPage';
-import { OutlinedInput, IconButton, Autocomplete, TextField, Typography, Box, CardHeader, CardContent, CardActions, Stepper, Step, StepLabel, FormControl, FormControlLabel, Checkbox, FormGroup, FormLabel, List, ListItem, ListItemText, Menu, MenuItem, InputLabel, ListSubheader, Select } from '@mui/material'
+import { OutlinedInput, IconButton, Autocomplete, TextField, Typography, Box, CardHeader, CardContent, CardActions, Stepper, Step, StepLabel, FormControl, FormControlLabel, Checkbox, FormGroup, FormLabel, List, ListItem, ListItemText, Menu, MenuItem, InputLabel, ListSubheader, Select, Button } from '@mui/material'
 import { makeStyles } from '@mui/styles';
 import HighlightOffRoundedIcon from '@mui/icons-material/HighlightOffRounded';
 import TopicChip from '../TopicChip'
@@ -12,32 +12,91 @@ import taxonomy from "../../taxonomy.json";
 const initialNeeds = JSON.parse(JSON.stringify(taxonomy.at(0).subLevels))
 const initialTech = JSON.parse(JSON.stringify(taxonomy.at(1).subLevels))
 
-/* const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles((theme) => ({
     root: {
         "& .MuiFormControl-root": {
 
             margin: theme.spacing(2),
         },
     },
-})); */
+}));
 
-
-
-const VerifySubmit = () => {
-    //const classes = useStyles();
-
-    const { values, handleCheckboxChange, convertToEventParams } = useContext(FormContext)
+const VerifySubmit = ({ onNextStep, onBackStep, selectedTopics, setSelectedTopics, formDataVerify1, setFormDataVerify1, taxonomy }) => {
+    const classes = useStyles();
+    const { values, handleCheckboxChange, convertToEventParams } = useContext(FormContext);
     const [selectedEl, setSelectedEl] = useState([]);
     const [isEl, setIsEl] = useState(false);
 
+    // Stato per tenere traccia dell'eventuale errore di validazione
+    const [validationError, setValidationError] = useState(false);
+
+    // Funzione per gestire il click sul pulsante "Next"
+    const handleNextStep = () => {
+      onNextStep();
+  };
+
+  // Funzione per gestire il click sul pulsante "Back"
+  const handleBackStep = () => {
+    onBackStep();
+  };
+
+  const handleTopicSelectionChange = (event) => {
+    selectedTopics = event.target.value;
+    setSelectedTopics(selectedTopics);
+  };
+
+    const handleTopicCheckboxChange = (event, taxonomy) => {
+      if (event.target.checked) {
+        setSelectedTopics([...selectedTopics, taxonomy]);
+      } else {
+        setSelectedTopics(selectedTopics.filter((selectedTopic) => selectedTopic !== taxonomy));
+      }
+    };
+  
+    const renderRelatedTopics = (categories, isSubLevel = false) => {
+      if (!categories) {
+        return null;
+      }
+  
+      return categories.map((category) => {
+        const { label, subLevels } = category;
+  
+        return (
+          <React.Fragment key={label}>
+            {!isSubLevel ? (
+              <FormControlLabel
+              label={`- ${label}`}
+              disabled
+              control={
+                <Typography variant="body1" component="span">
+                  {selectedTopics.includes(label) ? "[X]" : "[ ]"}
+                </Typography>
+              }
+            />
+            ) : (
+              <FormControlLabel
+            control={
+              <Checkbox
+                checked={selectedTopics.includes(label)}
+                onChange={(e) => handleTopicCheckboxChange(e, label)}
+              />
+            }
+            label={label}
+          />            
+            )}
+            {subLevels && renderRelatedTopics(subLevels, true)}
+          </React.Fragment>
+        );
+      });
+    };
 
 
     const renderForm = (el, branch) => {
-        //console.log(Object.values(el))
+        console.log(Object.values(el))
 
         let name = Object.keys(el);
         let value = Object.values(el)
-        //console.log(name + value + branch)
+        console.log(name + value + branch)
         return (
             <FormControlLabel
                 key={name}
@@ -48,49 +107,42 @@ const VerifySubmit = () => {
                         onChange={e => handleCheckboxChange(branch, convertToEventParams(name, e.target.checked))}
                         value={value} />} />
         )
+  
     }
+
     return (
-        <>
+      <form autoComplete="off">
             <CardHeader
-                title="Check inside database"
-                subheader="Check explanation..."
+                title="Select the relative topics from the"
+                subheader="Choose one or more topics for your article"
             />
             <CardContent>
-                <Box sx={{ my: 2 }}>
-                    <p>Found in the taxonomy</p>
-                    <Box sx={{
-                        width: '80%',
-                        margin: "0 auto",
-                        overflow: 'auto',
-                        maxHeight: 400,
-                        '& ul': { padding: 0 },
-                    }}>
-                        <FormControl>
-                            <FormLabel>Needs</FormLabel>
-                            <FormGroup>
-                                {values.needs.map(el => renderForm(el, "needs"))}
-                            </FormGroup>
-                            <FormLabel>Technologies</FormLabel>
-                            <FormGroup>
-                                {values.tech.map(el => renderForm(el, "tech"))}
-                            </FormGroup>
-                        </ FormControl>
-                    </Box>
-
-                </Box>
+              <Box sx={{ my: 2 }}>
+                <p>Select topics</p>
+                  <Box
+                    sx={{
+                      width: '100%',
+                      margin: "0 auto",
+                      overflow: 'auto',
+                      maxHeight: 500,
+                      '& ul': { padding: 0 },
+                    }}
+                  >
+                  <FormControl>
+                  <FormLabel>Needs</FormLabel>
+                  <FormGroup>{renderRelatedTopics(taxonomy?.[0]?.subLevels)}</FormGroup>
+                  <FormLabel>Technologies</FormLabel>
+                  <FormGroup>{renderRelatedTopics(taxonomy?.[1]?.subLevels)}</FormGroup> 
+                  </FormControl>
+              </Box>
+            </Box>
+              <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Button onClick={handleBackStep}>Back</Button>
+                <Button onClick={handleNextStep}>Next</Button>
+              </Box>
             </CardContent>
-            <CardActions>
-            </CardActions>
-        </>
+        </form>
     )
 }
-
-
-
-
-
-
-
-
 
 export default VerifySubmit
